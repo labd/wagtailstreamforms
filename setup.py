fb96c67df3af200a4154d835d9a1002a139fe280
Stuart George <stuart.bigmassa@gmail.com>
2017-10-11 16:08:40 +0100

    circle ci

--- setup.py
+++ setup.py
@@ -14,12 +14,12 @@ setup(
         'wagtail_streamforms'
     ],
     install_requires=[
-        'Django',
-        'django-appconf',
-        'django-model-utils',
-        'django-multi-email-field',
-        'django-recaptcha',
-        'wagtail'
+        'Django>=1.8.1,<1.12',
+        'django-appconf>=1.0.2',
+        'django-model-utils>=3.0.0',
+        'django-multi-email-field>=0.5.1',
+        'django-recaptcha>=1.3.1',
+        'wagtail>=1.12,<2'
     ],
     include_package_data=True,
     keywords=['accent', 'design'],
