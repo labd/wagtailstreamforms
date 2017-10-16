9e0b35ac0906b63b5f93d895e78ba8779bffc386
Stuart George <stuart.bigmassa@gmail.com>
2017-10-16 14:14:39 +0100

    moved email partial to EmailForm class

--- wagtailstreamforms/models/form.py
+++ wagtailstreamforms/models/form.py
@@ -1,23 +1,20 @@
 import json
+import six
 
+from django.core.mail import send_mail
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from model_utils.managers import InheritanceManager
 from modelcluster.models import ClusterableModel
-from wagtail.wagtailadmin.edit_handlers import (
-    FieldPanel,
-    InlinePanel,
-    TabbedInterface,
-    ObjectList
-)
-
-from .partials import EmailPartial
-from .submission import FormSubmission
+from multi_email_field.fields import MultiEmailField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, TabbedInterface, ObjectList
 from wagtailstreamforms.conf import settings
 from wagtailstreamforms.forms import FormBuilder
 from wagtailstreamforms.utils import recaptcha_enabled
+
+from .submission import FormSubmission
 
 
 class BaseForm(ClusterableModel):
@@ -76,8 +73,13 @@ class BaseForm(ClusterableModel):
         return self.form_fields.all()
 
     def get_data_fields(self):
-        data_fields = [('submit_time', _('Submission date')), ]
-        data_fields += [(field.clean_name, field.label) for field in self.get_form_fields()]
+        data_fields = [
+            ('submit_time', _('Submission date')),
+        ]
+        data_fields += [
+            (field.clean_name, field.label)
+            for field in self.get_form_fields()
+        ]
         return data_fields
 
     def get_form_class(self):
@@ -109,14 +111,48 @@ class BasicForm(BaseForm):
     pass
 
 
-class EmailForm(BaseForm, EmailPartial):
+class EmailForm(BaseForm):
+    subject = models.CharField(
+        max_length=255
+    )
+    from_address = models.EmailField()
+    to_addresses = MultiEmailField()
+    message = models.TextField()
+    fail_silently = models.BooleanField(
+        default=True
+    )
+
+    email_panels = [
+        FieldPanel('subject', classname="full"),
+        FieldPanel('from_address', classname="full"),
+        FieldPanel('to_addresses', classname="full"),
+        FieldPanel('message', classname="full"),
+        FieldPanel('fail_silently'),
+    ]
 
     edit_handler = TabbedInterface([
         ObjectList(BaseForm.settings_panels, heading='General'),
         ObjectList(BaseForm.field_panels, heading='Fields'),
-        ObjectList(EmailPartial.panels, heading='Email Submission'),
+        ObjectList(email_panels, heading='Email Submission'),
     ])
 
     def process_form_submission(self, form):
         super(EmailForm, self).process_form_submission(form)
         self.send_form_mail(form)
+
+    def send_form_mail(self, form):
+        content = [self.message, '', ]
+
+        for name, field in form.fields.items():
+            data = form.cleaned_data.get(name)
+            if name == 'recaptcha' or not data:
+                continue
+            content.append(field.label + ': ' + six.text_type(data))
+
+        send_mail(
+            self.subject,
+            '\n'.join(content),
+            self.from_address,
+            self.to_addresses,
+            self.fail_silently
+        )
