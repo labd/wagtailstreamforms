a301ab30c6a0142673152f9bc2887c37ba90e21a
Stuart George <stuart.bigmassa@gmail.com>
2017-10-16 18:40:10 +0100

    fix bug invalid literal for int() in form chooser

--- tests/blocks/test_form_chooser_block.py
+++ tests/blocks/test_form_chooser_block.py
@@ -1,5 +1,5 @@
 from wagtailstreamforms.blocks import FormChooserBlock
-from wagtailstreamforms.models import BasicForm
+from wagtailstreamforms.models import BaseForm
 
 from ..test_case import AppTestCase
 
@@ -7,10 +7,34 @@ from ..test_case import AppTestCase
 class TestFormChooserBlockTestCase(AppTestCase):
 
     def setUp(self):
-        self.form = BasicForm.objects.create(
+        self.form = BaseForm.objects.create(
             name='Some Form',
             template_name='streamforms/form_block.html'
         )
+
+    def test_value_for_form(self):
+        block = FormChooserBlock()
+
+        self.assertEquals(block.value_for_form(self.form.pk), self.form.pk)
+        self.assertEquals(block.value_for_form(self.form), self.form.pk)
+
+    def test_value_from_form(self):
+        block = FormChooserBlock()
+
+        # possibly a bug in wagtail as not choosing a value and submitting
+        # raises invalid literal for int() with base 10: ''
+        self.assertIsNone(block.value_from_form(''))
+
+        self.assertTrue(isinstance(block.value_from_form(self.form.pk), self.form.__class__))
+        self.assertTrue(isinstance(block.value_from_form(self.form), self.form.__class__))
+
+    def test_to_python(self):
+        block = FormChooserBlock()
+
+        self.assertIsNone(block.to_python(None))
+        self.assertIsNone(block.to_python(100))
+
+        self.assertTrue(isinstance(block.to_python(self.form.pk), self.form.__class__))
 
     def test_form_render(self):
         block = FormChooserBlock()
