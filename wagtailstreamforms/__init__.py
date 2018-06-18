d3e4f6a179542f021a1e21fcc269f4e6ebf2bbda
Stuart George <stuart@accentdesign.co.uk>
2018-06-18 14:02:35 +0100

    bump version to 3.0 final

--- wagtailstreamforms/__init__.py
+++ wagtailstreamforms/__init__.py
@@ -2,6 +2,6 @@ from wagtailstreamforms.utils.version import get_version
 
 # major.minor.patch.release.number
 # release must be one of alpha, beta, rc, or final
-VERSION = (3, 0, 0, 'rc', 2)
+VERSION = (3, 0, 0, 'final', 0)
 
 __version__ = get_version(VERSION)
