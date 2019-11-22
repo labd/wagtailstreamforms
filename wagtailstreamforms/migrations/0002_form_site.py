32936bbd8fb8d4c75568488de221a2870f942d3e
Rob Moorman <rob@moori.nl>
2019-11-22 13:45:12 +0100

    Remove latest wagtailcore migration dependency

--- wagtailstreamforms/migrations/0002_form_site.py
+++ wagtailstreamforms/migrations/0002_form_site.py
@@ -7,7 +7,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wagtailcore', '0041_group_collection_permissions_verbose_name_plural'),
         ('wagtailstreamforms', '0001_initial'),
     ]
 
