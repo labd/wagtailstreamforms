57ff036593b43f4dd7926412d0075cefb934e5e5
Tim Kamanin <tim@timonweb.com>
2020-02-07 13:43:26 +0100

    Adds Django 3.0 support
    Django 3.0 removes `context` argument from `from_db_value` method. To support both Django 2.x and 3.0 I've added wildcard `*args` and `**kwargs`. These should be dropped too once `wagtailstreamforms` stops supporting Django 2.x

--- wagtailstreamforms/fields.py
+++ wagtailstreamforms/fields.py
@@ -145,7 +145,7 @@ class HookSelectField(models.Field):
         defaults.update(kwargs)
         return super().formfield(**defaults)
 
-    def from_db_value(self, value, expression, connection, context):
+    def from_db_value(self, value, expression, connection, *args, **kwargs):
         if value is None or value == '':
             return []
         return value.split(',')
