18bc760548a0c48800e9b3f90e85663dd1abadc7
V <10897799+VdeJong@users.noreply.github.com>
2022-08-08 16:20:14 +0200

    Update get_options method for multiple fields (#197)
    * Update get_options method for CheckboxesField, RadioField, MultiSelectField and DropdownField
    
    To create the choice options form the block_value, it gets the value. Yet, sometimes this block_value is a list with dicts with the value. Now it checks if this is the case, and gets the value from there instead of just the item of the list.

--- tests/urls.py
+++ tests/urls.py
@@ -1,5 +1,5 @@
-from django.urls import include, re_path
 from django.contrib import admin
+from django.urls import include, re_path
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 
