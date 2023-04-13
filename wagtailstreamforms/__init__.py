c8a60fe6bc3522bc8c36733086f7c62bd1114ba5
Paul J Stevens <p.stevens@labdigital.nl>
2023-04-13 16:23:21 +0200

    cleanup non-pep version hack

--- wagtailstreamforms/__init__.py
+++ wagtailstreamforms/__init__.py
@@ -1,7 +1,1 @@
-from wagtailstreamforms.utils.version import get_version
-
-# major.minor.patch.release.number
-# release must be one of alpha, beta, rc, or final
-VERSION = (4, 0, 0, "final", 1)
-
-__version__ = get_version(VERSION)
+__version__ = "4.0.0"
