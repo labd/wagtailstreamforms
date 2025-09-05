cdeb51dd85315209e286043ae64a0bcfaad23272
vsakkasld <v.sakkas@labdigital.nl>
2025-09-05 13:28:52 +0200

    Revert invalid fix

--- wagtailstreamforms/blocks.py
+++ wagtailstreamforms/blocks.py
@@ -27,11 +27,7 @@ class FormChooserBlock(blocks.ChooserBlock):
         return WagtailStreamFormsChooser()
 
     def get_form_state(self, value: dict):
-        try:
-            prep_value = self.get_prep_value(value)
-            return self.widget.get_value_data(prep_value)
-        except Exception:
-            return self.widget.get_value_data(value)
+        return self.widget.get_value_data(value)
 
 
 class WagtailFormBlock(blocks.StructBlock):
